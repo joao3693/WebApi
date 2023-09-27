c2cb734bb50f12c38e050962fed783c4a72fa808
Joao Wictor <joaowictor369@gmail.com>
2023-09-26 23:07:16 -0300

    Atualizando

---
+++ src/components/ListDrinks.vue
@@ -0,0 +1,21 @@
+<script setup>
+const drink = defineProps(["name","baseUrlSvg"])
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card p-2 mb-3">
+            <p class="text-center">{{strDrink.name}}</p>
+            <img
+            :src="baseUrlSvg" class="card-img-top"
+            alt="..."
+            height="80"
+            />
+        </div>
+    </div>
+    
+</template>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { onMounted, reactive, ref } from 'vue';
+import ListDrinks from '../components/ListDrinks.vue'
+
+let baseUrlSvg = ref("www.thecocktaildb.com/api/json/v1/1/search.php?f=m")
+let strDrink = reactive(ref())
+
+onMounted(() => {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=m")
+    .then(response => response.json())
+    .then(response => {
+      strDrink.value = response.results
+      console.log(strDrink);
+    });
+})
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <img src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/2x8thr1504816928.jpg" class="card-img-top" alt="...">
+            <div class="card body">
+              <h5 class="card-title">Mojito</h5>
+              <p class="card-text">haha</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6">
+          <div class="card" >
+            <div class="card-body-row">
+            <ListDrinks 
+                v-for="strDrink in strDrinks"
+                :key="strDrink.name"
+                :name="strDrink.name"
+                :baseUrlSvg="baseUrlSvg + strDrink.url.split('/')[6] + '.svg'"
+                />
+              {{ strDrink.name }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
